2aefcdd83e67e3150178005b9a3bc918455dbf3c
lboecken <78748591+lboecken@users.noreply.github.com>
2021-07-25 17:22:42 -0400

    switched to postgresql due to issues with sqlite & heroku.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import ClassManager
 
 def main():
     user = ('John', '4045094520', 'EST')
-    new_message = (2, '608 Lauder Circle, Woodstock, GA', 'HOME', 34.08528518676758, -84.45832061767578, 'DAILY', 1,1,1,1,1,1,1, '17:15')
+    new_message = (2, '608 Lauder Circle, Woodstock, GA', 'HOME', 34.08528518676758, -84.45832061767578, 'DAILY', 1,1,1,1,1,1,1, '17:20')
     ClassManager.class_manager.databasemanager.add_new_message_for_user_to_db(new_message)
     ClassManager.class_manager.databasemanager.add_user(user)
 
